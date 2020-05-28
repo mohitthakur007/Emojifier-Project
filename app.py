0b6a9d6b3f69ad500bda1ad8bd6255441a4f83c4
mohitthakur007 <41528050+mohitthakur007@users.noreply.github.com>
2020-05-28 17:11:56 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, request
+import predictor as pr
+
+
+
+
+# __name__ == __main__
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def hello():
+	return render_template("index.html" )
+
+@app.route('/about')
+def about():
+	return "<h1> About Page </h1>"
+
+
+@app.route('/home')
+def home():
+	return redirect('/')
+
+
+@app.route('/submit', methods = ['POST'])
+def submit_data():
+	if request.method == 'POST':
+		no1 = str(request.form['no1'])
+	return pr.predict(no1)
+
+
+
+
+if __name__ == '__main__':
+	# app.debug = True
+	app.run(debug = True)
